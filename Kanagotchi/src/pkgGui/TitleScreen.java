57f358e5719ee2b2e8feb465348ea1808768ef86
darkmet98 <darkmet98@gmail.com>
2019-04-24 10:53:14 +0200

    :sunglasses: Uploaded sources

---
+++ Kanagotchi/src/pkgGui/TitleScreen.java
@@ -0,0 +1,43 @@
+package pkgGui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class TitleScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TitleScreen window = new TitleScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TitleScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
