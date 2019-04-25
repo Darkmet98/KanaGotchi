d9b290ea3d500f47b01badd18f632539af5846d0
darkmet98 <darkmet98@gmail.com>
2019-04-25 10:22:59 +0200

    :heart_eyes: Switched to IntelliJ and JavaFX

--- Kanagotchi/src/pkgTests/Sample.java
+++ Kanagotchi/src/pkgTests/Sample.java
@@ -1,14 +1,4 @@
 package pkgTests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-class Sample {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+public class Sample {
 }
