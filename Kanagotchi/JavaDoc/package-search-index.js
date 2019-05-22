4d7ad62ac0eaefa93bef00addc2d0e75fddb1c8d
darkmet98 <darkmet98@gmail.com>
2019-05-22 23:03:51 +0200

    :grin: Added javadoc.

---
+++ Kanagotchi/JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Engine.pkgDataBase"},{"l":"Engine.pkgDebug"},{"l":"Engine.pkgExceptions"},{"l":"Engine.pkgGames"},{"l":"Engine.pkgItems"},{"l":"Engine.pkgMechanics"},{"l":"Engine.pkgSaves"},{"l":"Engine.pkgTests"},{"l":"Executables"},{"l":"UI.pkgControllers"}]
